a25f5bb20d07e06c06eaa4a4a05af89ded62f114
FakeEmperor <fake_empire@mail.ru>
2014-11-11 12:08:07 +0300

    [RC] LabAPL3
    Redone APL3

--- LabAPL3/Program.cs
+++ LabAPL3/Program.cs
@@ -2,11 +2,123 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace LabAPL3
 {
+    
     class Program
     {
+       
+       static bool HasPathAlt(List<List<bool>> matrix, Point from, Point to){
+           List<Point> lp = new List<Point>(); //point of visited
+           Point NA = new Point(-1,-1);
+           List<Point> available = new List<Point>(); //point of visited
+           int i,u;
+           bool finished = false;
+           int rows = matrix.Count>0?matrix[0].Count:0,
+               lines = matrix.Count;
+           Point current = from, temp;
+           Point[] points = new Point[4];
+           //true - wall, false - path
+           if(matrix[from.Y][from.X] == true ||
+               matrix[to.Y][to.X] == true ) 
+                   return false;
+           int numPs = 0;
+           lp.Add(current); //as we visited current
+
+           do {
+               numPs = 0;
+               if (current.X == to.X && current.Y == to.Y)
+                   return true;
+               //check if all is done
+               for (i = 0; i < 4; ++i)
+               {
+                   
+                   temp = NA;
+                   points[i] = NA;
+                   
+                   switch (i)
+                   {
+                       case 0:
+                           if (current.Y + 1 < lines ) {
+                               temp.X = current.X;
+                               temp.Y = current.Y+1;
+                           }
+                           
+                           break;
+                       case 1:
+                           if (current.Y - 1 >= 0)
+                           {
+                               temp.X = current.X;
+                               temp.Y = current.Y - 1;
+                           }
+                           break;
+                       case 2:
+                           if (current.X + 1 < rows )
+                           {
+                               temp.X = current.X +1 ;
+                               temp.Y = current.Y;
+                           }
+                           break;
+                       case 3:
+                           if (current.X - 1 >= 0)
+                           {
+                               temp.X = current.X -1;
+                               temp.Y = current.Y;
+                               
+                           }
+                           break;
+                   }
+                   if (temp.X != -1 && matrix[temp.Y][temp.X] == false
+                       && lp.IndexOf( temp ) == -1 )
+                   {
+                       points[i] = temp;
+                       numPs++;
+                   }
+
+               }
+               if (numPs == 0)
+               {
+                   //check if has other paths
+                   if (available.Count > 0)
+                   {
+                       current = available[0];
+                       available.RemoveAt(0);
+                       lp.Add(current);
+                   }
+                   else
+                       finished = true;
+               }
+               else
+               {
+                   bool used_kokoko = false;
+                   for (i = 0; i < 4; ++i)
+                   {
+                       if (points[i].X != -1)
+                       {
+                           if (used_kokoko)
+                               //others - move to availables
+                               available.Add(points[i]);
+                           else
+                           {
+                               //move by first
+                               used_kokoko = true;
+                               current = points[i];
+                           }
+
+                       }
+                   }
+
+                   //fill visited with current
+                   lp.Add(current);
+               }
+               
+           } while( !finished );
+
+           return false;
+       }
+
         static void InDepthWalk(List<List<bool>> matrix, List<bool> visited, int from)
         {
             visited[from] = true;
@@ -31,16 +143,17 @@ namespace LabAPL3
             return false;
         }
 
-        static List<List<bool>> InputMatrix(out int from, out int to)
+        static List<List<bool>> InputMatrix(out Point from, out Point to)
         {
             List<List<bool>> matrix;
             string input;
             bool parsed = false;
             int a = -1;
+            int d = 0;
             int u = 0;
 
-            from = 0;
-            to = 0;
+            from = new Point(-1,-1);
+            to =new Point(-1,-1);
             Console.WriteLine("Write matrix size");
             do
             {
@@ -90,43 +203,55 @@ namespace LabAPL3
             Console.WriteLine("Path from A to B:");
             
             int numRead = 0;
-           
-            do
-            {
-                u = 0;
-                int i=0;
-                input = Console.ReadLine();
-                do
-                {
-                    for (i = u; i < input.Length; ++i)
-                        if (Char.IsWhiteSpace(input[i]))
+            for (d=0; d<2; ++d) {
+               Console.WriteLine("Path from point "+ (d==0?"A":"B") +":");
+               numRead = 0;
+               do
+               {
+                    u = 0;
+                    int i=0;
+                    input = Console.ReadLine();
+                    do
+                    {
+                        for (i = u; i < input.Length; ++i)
+                         if (Char.IsWhiteSpace(input[i]))
                             break;
-                    try
-                    {
-                        int temp = Int32.Parse(input.Substring(u, i - u));
-                        if (numRead == 0)
-                            from = temp;
-                        else
-                            to = temp;
-                        numRead++;
-                        u = i + 1;
+                        try
+                        {
+                            int temp = Int32.Parse(input.Substring(u, i - u));
+                            if (numRead == 0) {
+                                if (d == 0)
+                                    from.X = temp;
+                                else
+                                    to.X = temp;
+                            }
+                                
+                            else
+                                if (d == 0)
+                                    from.Y = temp;
+                                else
+                                    to.Y = temp;
+                            numRead++;
+                            u = i + 1;
 
-                    }
-                    catch (SystemException e)
-                    {
-                        Console.WriteLine("Parse Failed!");
-                    }
-                } while (i != input.Length);
-            } while (numRead < 2);
+                        }
+                        catch (SystemException e)
+                        {
+                            Console.WriteLine("Parse Failed!");
+                        }
+                    } while (i != input.Length);
+                } while (numRead < 2);
+            }
+           
 
             return matrix;
         }
 
         static void Main(string[] args)
         {
-            int a, b;
+            Point a, b;
             List<List<bool>> matrix = InputMatrix(out a, out b);
-            bool hp = HasPath(matrix, a, b);
+            bool hp = HasPathAlt(matrix, a, b);
             Console.Write("From A to B path has");
             if(!hp)
                 Console.Write(" not");
